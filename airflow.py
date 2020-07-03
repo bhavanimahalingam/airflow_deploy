190c19ebb1d8b0f4bfc22d53371cc05fd925eaac
bhavanimahalingam <58926863+bhavanimahalingam@users.noreply.github.com>
2020-07-03 12:21:35 +0530

    Update airflow.py

--- airflow.py
+++ airflow.py
@@ -10,5 +10,5 @@ def print_hello():
 dag = DAG('bavani_id', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+dummy1_operator = DummyOperator(task_id='dummy_task_15', retries = 3, dag=dag)
 dummy1_operator >> hello_operator
